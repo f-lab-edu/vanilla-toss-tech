c10b829f89316808e60b404345448cd0225b31e3
sydney-choi <soyeong0730@gmail.com>
2024-01-29 10:32:15 +0900

    feat: page작성

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import Router from './router.js';
+import Layout from './views/layout/index.js';
+import Main from './views/main/index.js';
+import Article from './views/article/index.js';
+import NotFound from './views/notFound/index.js';
+
+const createPages = (container) => {
+  const main = () => new Main(container);
+  const article = () => new Article(container);
+  const notFound = () => new NotFound(container);
+  return {
+    main,
+    article,
+    notFound,
+  };
+};
+
+const start = () => {
+  const container = document.querySelector('#app');
+  // eslint-disable-next-line no-new
+  new Layout(container);
+  const router = new Router(container);
+  const pages = createPages(container);
+  router.addRoute('/', pages.main);
+  router.addRoute('/tech', pages.main);
+  router.addRoute('/design', pages.main);
+  router.addRoute('/article/:id', pages.article);
+  router.setNotFound(pages.notFound);
+  router.start();
+};
+
+// TODO data 준비 되면 화면 띄우기
+document.addEventListener('DOMContentLoaded', start);
