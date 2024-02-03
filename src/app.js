8823ac51f2ed06ed0a43b8954679af34cbc17798
sydney-choi <soyeong0730@gmail.com>
2024-02-03 15:08:30 +0900

    fix: index.js로부터 app.js를 불러와 렌더링 할 수 있도록 수정

--- src/app.js
+++ src/app.js
@@ -1,36 +1,35 @@
 import Router from './router.js';
-import Layout from './views/layout/index.js';
-import Main from './views/main/index.js';
-import Article from './views/article/index.js';
-import NotFound from './views/notFound/index.js';
+import Home from './pages/index.js';
+import Article from './pages/article.js';
+import NotFound from './pages/notFound.js';
 
-const createPages = (container) => {
-  const main = () => new Main(container);
-  const article = () => new Article(container);
-  const notFound = () => new NotFound(container);
-  return {
-    main,
-    article,
-    notFound,
+export default () => {
+  const createPages = (container) => {
+    const home = () => new Home(container);
+    const article = () => new Article(container);
+    const notFound = () => new NotFound(container);
+    return {
+      home,
+      article,
+      notFound,
+    };
   };
-};
 
-const createRouter = (container) => {
-  const pages = createPages(container);
-  const router = new Router(container);
-  router.addRoute('/', pages.main);
-  router.addRoute('/tech', pages.main);
-  router.addRoute('/design', pages.main);
-  router.addRoute('/article/:id', pages.article);
-  router.setNotFound(pages.notFound);
-  return router;
-};
+  const createRouter = (container) => {
+    const pages = createPages(container);
+    const router = new Router(container);
+    router.addRoute('/', pages.home);
+    router.addRoute('/tech', pages.home);
+    router.addRoute('/design', pages.home);
+    router.addRoute('/article/:id', pages.article);
+    router.setNotFound(pages.notFound);
+    return router;
+  };
 
-const start = () => {
-  const container = document.querySelector('#app');
-  const layout = new Layout(container);
-  layout.render();
-  createRouter(container).start();
-};
+  const start = () => {
+    const container = document.querySelector('#app');
+    createRouter(container).start();
+  };
 
-document.addEventListener('DOMContentLoaded', start);
+  document.addEventListener('DOMContentLoaded', start);
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles/reset.css';
+import './styles/root.module.css';
+
+import app from './app.js';
+
+app();
